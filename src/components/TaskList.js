a543741e6d48cd3808f898b798130ac0fef814c0
Elvis <elvis.omondi@student.moringaschool.com>
2024-09-14 23:16:42 +0300

    commit

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,24 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, onTaskDelete }) {
+  const handleDelete = (taskToDelete) => {
+    onTaskDelete((prevTasks) => prevTasks.filter(task => task !== taskToDelete));
+  };
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task, index) => (
+        <Task 
+          key={index}
+          text={task.text}
+          category={task.category}
+          onDelete={() => handleDelete(task)}
+        />
+      ))}
     </div>
   );
 }
 
 export default TaskList;
+
